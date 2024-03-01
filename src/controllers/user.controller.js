41058b9054c4513750573ec807ca6822d9d990a4
rohit <rohitsirsat855@gmail.com>
2024-03-02 00:59:47 +0530

    fix create user and register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,7 +16,7 @@ const registerUser = asyncHandler(async (req, res) => {
   // return res
 
   const { fullName, email, username, password } = req.body;
-  console.log("email: ", email);
+  // console.log("email: ", email);
 
   // if (fullName === "") {
   //   throw new ApiError(400, "fullname is required");
@@ -30,7 +30,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   // check if user already exists: username, email
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
 
@@ -40,9 +40,19 @@ const registerUser = asyncHandler(async (req, res) => {
 
   // check for images, check for avatar
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
+
   if (!avatarLocalPath) {
-    throw new ApiError(400, "Avatar file is required");
+    throw new ApiError(400, "Avatar file is requiredd");
   }
 
   // upload them to cloudinary, check for avatar
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -17,7 +17,9 @@ const uploadOnCloudinary = async (localFilePath) => {
     });
 
     // file has been uploaded successfully
-    console.log("file is uploaded on cloudinary", response.url);
+    // console.log("file is uploaded on cloudinary", response.url);
+    // console.log(response);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     // remove the locally saved temporary file as the upload opration got failed
