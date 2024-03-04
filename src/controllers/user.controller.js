40742fdcdc59f85b01832a7dbdf5c279a456a7b6
rohit <rohitsirsat855@gmail.com>
2024-03-04 19:53:24 +0530

    fix bug in getUserChannelProfile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -170,8 +170,8 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined,
+      $unset: {
+        refreshToken: 1, // this removes the field from document
       },
     },
     {
@@ -358,11 +358,86 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "Cover image updated successfully"));
 });
 
+// some bug in my getUserChannelProfile method
+// const getUserChannelProfile = asyncHandler(async (req, res) => {
+//   const { username } = req.params;
+
+//   if (!username?.trim()) {
+//     throw new ApiError(400, "user is missing");
+//   }
+
+//   const channel = await User.aggregate([
+//     {
+//       $match: {
+//         username: username?.toLowerCase(),
+//       },
+//     },
+//     {
+//       $lookup: {
+//         from: "subscriptions",
+//         localField: "_id",
+//         foreignField: "channel",
+//         as: "subscribers",
+//       },
+//     },
+//     {
+//       $lookup: {
+//         from: "subscriptions",
+//         localField: "_id",
+//         foreignField: "subscriber",
+//         as: "subscribedTo",
+//       },
+//     },
+//     {
+//       $addFields: {
+//         subscribersCount: {
+//           // $size: "$subscribers",
+//           $size: "$subscribers",
+//         },
+//         channelSubscribedToCount: {
+//           $size: "subscribedTo",
+//         },
+//         isSubscribed: {
+//           $cond: {
+//             if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+//             then: true,
+//             else: false,
+//           },
+//         },
+//       },
+//     },
+//     {
+//       $project: {
+//         fullName: 1,
+//         username: 1,
+//         subscribersCount: 1,
+//         channelSubscribedToCount: 1,
+//         isSubscribed: 1,
+//         avatar: 1,
+//         coverImage: 1,
+//         email: 1,
+//       },
+//     },
+//   ]);
+//   if (!channel?.length) {
+//     throw new ApiError(404, "channel does not exists");
+//   }
+
+//   return res
+//     .status(200)
+//     .json(
+//       new ApiResponse(200, channel[0], "User channel fetched successfully")
+//     );
+
+//   console.log(channel);
+// });
+
+// HC sir's method
 const getUserChannelProfile = asyncHandler(async (req, res) => {
   const { username } = req.params;
 
   if (!username?.trim()) {
-    throw new ApiError(400, "user is missing");
+    throw new ApiError(400, "username is missing");
   }
 
   const channel = await User.aggregate([
@@ -392,8 +467,8 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
         subscribersCount: {
           $size: "$subscribers",
         },
-        channelSubscribedToCount: {
-          $size: "subscribedTo",
+        channelsSubscribedToCount: {
+          $size: "$subscribedTo",
         },
         isSubscribed: {
           $cond: {
@@ -409,7 +484,7 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
         fullName: 1,
         username: 1,
         subscribersCount: 1,
-        channelSubscribedToCount: 1,
+        channelsSubscribedToCount: 1,
         isSubscribed: 1,
         avatar: 1,
         coverImage: 1,
@@ -417,6 +492,7 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
       },
     },
   ]);
+
   if (!channel?.length) {
     throw new ApiError(404, "channel does not exists");
   }
@@ -426,8 +502,6 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     .json(
       new ApiResponse(200, channel[0], "User channel fetched successfully")
     );
-
-  // console.log(channel);
 });
 
 const getWatchHistory = asyncHandler(async (req, res) => {
